ec52b10bbedcfec6138e0587bb9e9de10deac783
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2023-03-06 17:15:00 -0800

    built react app and added components and tailwind

---
+++ my-portolio/src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function About(){
+    return(
+        <section id="about">
+            <div>
+                <h1>
+                    About Me!
+                </h1>
+                <p>
+                    My name is Alex Jaworski. I am a junior developer currently working on getting my full stack certification through the UCR Extensions bootcamp. I am excited to learn new technologies and enjoy new challenges. My goal is to find employment where I can take advantage of all the skills I've learned thus far. The greatest satisfaction in coding to me comes from overcoming hurdles and finding a creative and efficient way to do so. While I enjoy front end design I find back-end and server work far more enjoyable and look forward to furthering my skills in those areas.
+                </p>
+            </div>
+
+        </section>
+    )
+}
---
+++ my-portolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+export default function Footer(){
+    return(
+        <div>
+            <a href="https://github.com/Ajaws2022">
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+            </a>
+        </div>
+    )
+}
