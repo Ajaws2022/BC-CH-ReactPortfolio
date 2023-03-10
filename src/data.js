84e6146395c106a7b97eccf2b428c7ab60bf8812
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2023-03-09 21:31:16 -0800

    attempting to fix image routes

--- src/data.js
+++ src/data.js
@@ -1,45 +1,51 @@
 export const projects = [
-    
+    {
+        title: "{Love.Dev}",
+        subtitle: "Node, MongoDb, React, Apollo, GraphQL",
+        description: "A dating app by developers for developers",
+        image: "/images/LoveLogo.png",
+        link: "https://github.com/BaileyM96/Love.Dev",
+    },
     {
         title: "Skaro",
         subtitle: "Node, Express, Handlebars",
         description: "A project management dashboard built using the MVC framework",
-        image: "./images/projectManager.png",
+        image: "/images/projectManager.png",
         link: "https://github.com/DanielMartin971/Skaro",
     },
     {
         title: "SQL Employee Database",
         subtitle: "SQL, MySql, Inquirer",
         description: "An employee database manager using the terminal and MySql",
-        image: "./images/employeeSQL.png",
+        image: "/images/employeeSQL.png",
         link: "https://github.com/Ajaws2022/BC-CH-SQL_Employee_Data",
     },
     {
         title: "Social API",
         subtitle: "MongoDB, Mongoose, Express",
         description: "A mock social network API built using MongoDB, Mongoose, and Express",
-        image: "./images/SocialAPI.png",
+        image: "/images/SocialAPI.png",
         link: "https://github.com/Ajaws2022/BC-CH-SocialAPI",
     },
     {
         title: "Node Notes",
         subtitle: "Node & Express",
         description: "A Simple Note Taker app built using express",
-        image: "./images/noteTaker.png",
+        image: "/images/noteTaker.png",
         link: "https://github.com/Ajaws2022/BC-CH-9-NodeNotes",
     },
     {
         title: "Time Tracker",
         subtitle: "Node, HTML, Javascript",
         description: "A simple time tracker that highlights past and present hours",
-        image: "./images/Planner.png",
+        image: "/images/Planner.png",
         link: "https://ajaws2022.github.io/Stuff-Things-Times-Tracker/",
     },
     {
         title: "Random Password Generator",
         subtitle: "Javascript, HTML, CSS",
         description: "A simple password generator that accepts user input to determine length and characters",
-        image: ".//images/PassGenerator.png",
+        image: "/images/PassGenerator.png",
         link: "https://ajaws2022.github.io/BC-CH-Gen.Pass./",
     }
 ]
